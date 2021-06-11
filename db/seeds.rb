fc76ef0b0608484c2b79ccac335d01c621320d86
Renan Lapazini <renanlapazinisocialmedia@gmail.com>
2021-06-11 20:32:01 -0300

    Populate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CallCost.create(origin: "011", receiver: "016", price:1.90)
+CallCost.create(origin: "016", receiver: "011", price:2.90)
+CallCost.create(origin: "011", receiver: "017", price:1.70)
+CallCost.create(origin: "017", receiver: "011", price:2.70)
+CallCost.create(origin: "011", receiver: "018", price:0.90)
+CallCost.create(origin: "018", receiver: "011", price:1.90)
+
+Plan.create(name: "FaleMais 30", price: 10, minutes:30)
+Plan.create(name: "FaleMais 60", price: 15, minutes:60)
+Plan.create(name: "FaleMais 120", price: 25, minutes:120)
