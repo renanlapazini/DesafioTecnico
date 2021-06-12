4650fdc3e2f2323474a9eb6f32f590571971a435
Renan Lapazini <renanlapazinisocialmedia@gmail.com>
2021-06-12 04:03:54 -0300

    Add calculate cost action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,27 @@
 class HomeController < ApplicationController
-  def index
+  before_action :load_infos, only: %i[index calculate_cost]
+
+  def index; end
+
+  def calculate_cost
+    @cost_without_plan = calculator_service[:cost_without_plan]
+    @cost_with_plan = calculator_service[:cost_with_plan]
+  end
+
+  private
+
+  def load_infos
     @all_origins = CallCost.all.pluck(:origin).uniq.sort
     @all_receivers = CallCost.all.pluck(:receiver).uniq.sort
     @all_plans = Plan.all.pluck(:name)
   end
+
+  def calculator_service
+    @calculator_service ||= CostCalculator.new(params["origin"], params["receiver"], params["minutes"], find_plan).call
+  end
+
+  def find_plan
+    Plan.find_by(name: params["plan"])
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index' 
 
-  resources :home, only: %i[index]
+  resources :home, only: %i[index] do 
+    collection do
+      get :calculate_cost 
+    end
+  end
 end
